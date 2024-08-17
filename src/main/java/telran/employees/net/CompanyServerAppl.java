7350d9e7cf28b309a1fddbc34fa684388bb0d070
AlekseiKorotkov159 <158587440+AlekseiKorotkov159@users.noreply.github.com>
2024-08-17 19:46:27 +0300

    race condition solution based on Company specific implementation

--- src/main/java/telran/employees/net/CompanyServerAppl.java
+++ src/main/java/telran/employees/net/CompanyServerAppl.java
@@ -1,26 +1,53 @@
 package telran.employees.net;
 
+import java.util.Scanner;
+
 import telran.employees.*;
 import telran.io.Persistable;
 import telran.net.Protocol;
 import telran.net.TcpServer;
-
-public class CompanyServerAppl {
+import static telran.net.TcpConfigurationProperties.*
+;public class CompanyServerAppl {
 
 	private static final String FILE_NAME = "employeesTest.data";
 	private static final int PORT = 5000;
-
+	
 	public static void main(String[] args) {
 		
 		Company company = new CompanyMapsImpl();
-		try {
-			((Persistable)company).restore(FILE_NAME);
-		} catch (Exception e) {
-			
+		Persistable persistable = null;
+		if(company instanceof Persistable) {
+			persistable = (Persistable)company;
+		}
+		if (persistable != null) {
+			try {
+				persistable.restore(FILE_NAME);
+			} catch (Exception e) {
+
+			} 
 		}
 		Protocol protocol = new CompanyProtocol(company);
 		TcpServer tcpServer = new TcpServer(protocol, PORT);
-		tcpServer.run();
+		Thread serverThread = new Thread(tcpServer);
+		serverThread.start();
+		boolean running = true;
+		try (Scanner scanner = new Scanner(System.in);){
+			while(running) {
+				System.out.println(String.format("Enter %s for the server shutdown", SHUTDOWN));
+				String line = scanner.nextLine();
+				if (line != null && line.equals(SHUTDOWN)) {
+					running = false;
+					tcpServer.shutdown();
+					serverThread.join(); //make sure all TCP related threads have been finished
+					if (persistable != null) {
+						persistable.save(FILE_NAME);
+					}
+				}
+			}
+		} 
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 
 	}
 
